046b426ac270faf269bf5f6d14306225fb11e6a5
Nikita <mipo4t@gmail.com>
2018-10-06 17:52:49 +0400

    add mount and serve static files

--- index.js
+++ index.js
@@ -5,11 +5,54 @@
 import path from 'path';
 import _ from 'lodash';
 import Koa from 'koa';
+import serve from 'koa-static';
+import mount from 'koa-mount';
 import Pug from 'koa-pug';
-// import container from './container';
+import container from './container';
 
 export default () => {
   const app = new Koa();
+  const log = container.logger;
+
+  app.use(mount('/assets', serve(path.join(__dirname, 'dist'))));
+
+  app.use(async (ctx, next) => {
+    try {
+      await next();
+    } catch (err) {
+      // will only respond with JSON
+      ctx.status = err.statusCode || err.status || 500;
+      ctx.body = {
+        message: err.message,
+      };
+      if (ctx.status === 404) {
+        ctx.render('404');
+      }
+      ctx.render('500');
+    }
+  });
+
+  // x-response-time
+  app.use(async (ctx, next) => {
+    const start = new Date();
+    await next();
+    const ms = new Date() - start;
+    log(`1: ${ctx.method} ${ctx.url} - ${ms}`);
+  });
+
+  // logger
+  app.use(async (ctx, next) => {
+    const start = new Date();
+    await next();
+    const ms = new Date() - start;
+    log(`2: ${ctx.method} ${ctx.url} - ${ms}`);
+  });
+
+  // response
+  app.use((ctx) => {
+    ctx.body = 'Hello World';
+    throw new Error('Oi blya-a-a');
+  });
 
   const pug = new Pug({
     viewPath: path.join(__dirname, 'views'),
@@ -17,7 +60,7 @@ export default () => {
     debug: true,
     pretty: true,
     compileDebug: true,
-    locals: [],
+    locals: [], // global_locals_for_all_pages
     basedir: path.join(__dirname, 'views'),
     helperPath: [
       { _ },
@@ -25,5 +68,6 @@ export default () => {
     ],
   });
   pug.use(app);
+
   return app;
 };
