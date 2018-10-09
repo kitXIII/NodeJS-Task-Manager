ceb29d27e3eebaa9b6c9eb516f941f060e6d3bce
Nikita <mipo4t@gmail.com>
2018-10-09 12:34:01 +0400

    fix

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -20,7 +20,7 @@ module.exports = {
       {
         test: /\.js$/,
         loader: 'babel-loader',
-        // exclude: '/node_modules/',
+        exclude: '/node_modules/',
       },
       {
         test: /\.min\.css$/,
