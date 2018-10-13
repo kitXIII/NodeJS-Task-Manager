767c66d786f44cdc8b181968c86aac53a047c206
Nikita <mipo4t@gmail.com>
2018-10-13 13:14:06 +0400

    change container

--- container.js
+++ container.js
@@ -1,12 +1,10 @@
 import dotenv from 'dotenv';
 import logger from './lib/logger';
 // import encrypt from './lib/secure';
-// import { User } from './models';
 
 dotenv.config();
 
 export default {
   logger,
   // encrypt,
-  // User,
 };
