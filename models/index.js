3c4fe0fc8c99aa98a97d591ec951cebc7cfe1eb1
Nikita <mipo4t@gmail.com>
2018-10-14 17:20:44 +0400

    add logger as logging option to sequelize

--- models/index.js
+++ models/index.js
@@ -2,18 +2,23 @@ import fs from 'fs';
 import path from 'path';
 import Sequelize from 'sequelize';
 import confFromFile from '../config/config.json';
+import container from '../container';
 
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
+const { logger } = container;
 const config = confFromFile[env];
 const db = {};
 
 let sequelize;
 
 if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+  sequelize = new Sequelize(process.env[config.use_env_variable], { ...config, logging: logger });
 } else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
+  sequelize = new Sequelize(config.database, config.username, config.password, {
+    ...config,
+    logging: logger,
+  });
 }
 
 fs
