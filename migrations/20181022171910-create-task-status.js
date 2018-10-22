24888ab164ceff0109d4ddf268a4df86c912a824
Nikita <mipo4t@gmail.com>
2018-10-22 22:28:16 +0400

    add task status model and migration

---
+++ migrations/20181022171910-create-task-status.js
@@ -0,0 +1,24 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => queryInterface.createTable('taskStatuses', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: Sequelize.INTEGER,
+    },
+    name: {
+      unique: true,
+      allowNull: false,
+      type: Sequelize.STRING,
+    },
+    createdAt: {
+      allowNull: false,
+      type: Sequelize.DATE,
+    },
+    updatedAt: {
+      allowNull: false,
+      type: Sequelize.DATE,
+    },
+  }),
+  down: queryInterface => queryInterface.dropTable('taskStatuses'),
+};
---
+++ models/taskstatus.js
@@ -0,0 +1,27 @@
+import formatDate from '../lib/formatDate';
+
+module.exports = (sequelize, DataTypes) => {
+  const taskStatus = sequelize.define('taskStatus', {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        notEmpty: {
+          args: true,
+          msg: "This field can't be empty",
+        },
+      },
+    },
+  }, {
+    getterMethods: {
+      created() {
+        return formatDate(this.createdAt);
+      },
+    },
+  });
+  // taskStatus.associate = function(models) {
+  //   // associations can be defined here
+  // };
+  return taskStatus;
+};
